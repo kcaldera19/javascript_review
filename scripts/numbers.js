f42f5b6a576c356f7069f33b4fc1130d7b74cef2
Katalina Caldera <katalinacaldera19@gmail.com>
2024-05-07 14:58:50 -0500

    number

--- scripts/numbers.js
+++ scripts/numbers.js
@@ -1,2 +1,28 @@
 "use strict"
 console.log("whats up");
+
+// 1.
+let shoesPrice = 20.99;
+    console.log(shoesPrice);
+    // 2.
+    shoesPrice += 15;
+    console.log(shoesPrice);
+
+    // 3.
+    shoesPrice -= 8;
+    console.log(shoesPrice);
+
+// 4.
+let average = (26+44+72+86+112)/5;
+    console.log(average);
+// 5.
+// oraginal
+// let theNumber = 14;
+// console.log(theNumber++)
+// modify
+let theNumber = 14;
+console.log(++theNumber)
+
+// 6. special operator called typeof. tells you data type 
+
+
