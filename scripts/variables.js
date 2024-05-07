820932bbb177ffa50edeb72f7a8f12f9a071c5c4
Katalina Caldera <katalinacaldera19@gmail.com>
2024-05-07 13:50:43 -0500

    Variable review

--- scripts/variables.js
+++ scripts/variables.js
@@ -1,2 +1,32 @@
 "use strict"
-console.log("whats up")
+console.log("whats up");
+
+// value to variables
+let dog = 5;
+dog = 20;
+console.log("dog");
+
+const cat = 5;
+console.log("cat");
+
+var chicken = 6;
+console.log("chicken");
+
+// changed value in variable with let and var
+let price;
+price = 29.99;
+console.log(price);
+
+var wise= 20;
+wise = 30;
+console.log(wise);
+
+// const does not change once the value has been assigned
+const life = 40;
+// life = 20;
+console.log(life);
+// 4. If you try to change the variable of a const you will get Assignment to constant variable.
+// 5. if you dont want a value of varibale to change use const.
+// 6. Var wont show the error you expect.You could things out of order and not get an error.
+
+
