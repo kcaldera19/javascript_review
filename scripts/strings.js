f9f0d8f4ed96093b0f19465cee6ca1cb262d1c30
Katalina Caldera <katalinacaldera19@gmail.com>
2024-05-09 22:47:51 -0500

    strings review

--- scripts/strings.js
+++ scripts/strings.js
@@ -1,2 +1,19 @@
 "use strict"
 console.log("wow wow");
+
+
+
+// NOTES
+// if you want the quotes " " to show up you can put a \ blackslash or ` `
+let name = ' Andrea';
+message =`Hello${name}`;
+console.log(message);
+
+// NOTES
+// MANIPULATING STRINGS  by adding + wo
+let color = 'black';
+message = message.toLowerCase
+
+
+// let verb = 'drive';
+// let message =` `
